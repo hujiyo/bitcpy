3281f916e77928a7f257c3fac3f0f3baa2201205
hujiyo <hj18914255909@outlook.com>
2025-07-10 09:56:21 +0800

    Update bitcpy.h
    add extern "C" in head file

--- bitcpy.h
+++ bitcpy.h
@@ -1,5 +1,14 @@
+/*
+#CopyRight (c) HuJi 2025.1
+#Under MIT License
+#Email:Mhuixs@outlook.com
+*/
 #ifndef BITCPY_H
 #define BITCPY_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 #include <stdint.h>
 #include <string.h>
@@ -13,6 +22,10 @@ int bitcpy(uint8_t* destination, uint64_t dest_first_bit,
 void bitcpy_fast(uint8_t* destination, uint64_t dest_first_bit, 
                  const uint8_t* source, uint64_t source_first_bit, 
                  uint64_t len);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif // BITCPY_H
 
